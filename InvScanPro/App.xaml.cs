79bff0548097e524c4c496966de17a3c8cf4db0a
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2023-08-25 22:30:21 +0200

    Dodaj pliki projektów.

---
+++ InvScanPro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InvScanPro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ InvScanPro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InvScanPro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
