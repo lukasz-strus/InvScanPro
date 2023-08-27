44cfc9e2e522f4203d9805911eb20adbe1a0ed1e
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2023-08-27 15:24:07 +0200

    #6 Add date property. Add LoadFileCommand

--- src/InvScanPro/ViewModels/DateViewModel.cs
+++ src/InvScanPro/ViewModels/DateViewModel.cs
@@ -5,11 +5,18 @@ namespace InvScanPro.ViewModels;
 
 public partial class DateViewModel : ObservableObject
 {
-    public string Date { get; set; }
+    [ObservableProperty]
+    DateTime date = DateTime.Now; // TODO get date from file
 
     [RelayCommand]
     async Task NavigateToLocationPage()
     {
         //TODO create location page and navigate mechanism
+    }
+
+    [RelayCommand]
+    async Task LoadFile()
+    {
+        //TODO create load file mechanism
     }
 }
