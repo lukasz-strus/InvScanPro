8208948058e6bec5ecb78809f0d76e35efdeabf9
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2023-08-27 17:43:48 +0200

    #7 Add title labels. Add actions for button.

--- src/InvScanPro/ViewModels/LocationViewModel.cs
+++ src/InvScanPro/ViewModels/LocationViewModel.cs
@@ -1,7 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace InvScanPro.ViewModels;
 
-public class LocationViewModel : ObservableObject
+public partial class LocationViewModel : ObservableObject
 {
+    [RelayCommand]
+    async void NavigateToGeneralPage()
+    {
+        //TODO create general page and navigate mechanism
+    }
+
+    [RelayCommand]
+    async Task LoadFile()
+    {
+        //TODO create load file mechanism
+    }
 }
