7907d2159604514bafaaddec6696f8547111b444
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2023-09-19 21:43:41 +0200

    #17 Clear todo

--- src/InvScanPro/Services/CsvFileService.cs
+++ src/InvScanPro/Services/CsvFileService.cs
@@ -12,7 +12,6 @@ public interface ICsvFileService
 
 public class CsvFileService : ICsvFileService
 {
-    //TODO add Yes/No dialog to check is file in cache should be replaced
     public async Task<List<InventoryItem>> LoadCsvFileAsync()
     {
         var records = new List<InventoryItem>();
